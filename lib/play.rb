ca29084fe6faf60f466c69cd00112ac9f64b0aeb
Rafael Garcia <rafagrcb@gmail.com>
2020-03-23 19:12:50 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,65 @@ def turn(board)
 end
 
 # Define your play method below
+
+win_conditions = [[0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],]
+  
+def win_conditions
+end
+
+def over?(board)
+      if full?(board) == true
+        return true
+      end
+      return false
+end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
+
+def won?(board)
+  win_conditions do |win|
+  if win.all?{|y| board[y] == "X"}
+    return win
+    elsif win.all?{|y| board[y] == "O"}
+      return win
+    end
+  end
+  return false
+end
+
+def draw?(board)
+  !(won?(board)) && full?(board)
+end
+
+def play(board)
+  while !over?(board) && !won?(board) && !draw?(board)
+  turn(board)
+  end
+  if won?(board)
+    puts "Congratulations winner!"
+  end
+  if draw?(board)
+    puts "Draw"
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |index|
+    if index != " "
+      count += 1
+    end
+  end
+    count
+ end
